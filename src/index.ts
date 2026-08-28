132c1816ba5559f6442621ecf1e9021c71f9c12e
SULIMAN ALSHAMMARI <239743497+Grar00t@users.noreply.github.com>
2026-08-28 15:40:09 +0300

    refactor: expose Niyah runtime API

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './engine/model-provider';
+export * from './engine/ollama-provider';
+export * from './engine/classifier';
+export * from './engine/response-quality';
+export * from './engine/niyah-engine-v5';
